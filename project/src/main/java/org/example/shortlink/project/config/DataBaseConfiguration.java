3f04226e7701ab750a80fd1ad082c6e420e55ae4
cxlong <3371930743@qq.com>
2024-03-27 21:15:03 +0800

    提交MySQL数据库分页插件

---
+++ project/src/main/java/org/example/shortlink/project/config/DataBaseConfiguration.java
@@ -0,0 +1,18 @@
+package org.example.shortlink.project.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataBaseConfiguration {
+    /**
+     * 分页插件
+     */
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
