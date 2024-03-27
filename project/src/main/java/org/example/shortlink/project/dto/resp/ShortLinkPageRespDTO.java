8759c98735ae0ee1eb16682d12844553152b5b3a
cxlong <3371930743@qq.com>
2024-03-27 23:13:44 +0800

    短链接分页接口新增创建时间以及时间序列化

--- project/src/main/java/org/example/shortlink/project/dto/resp/ShortLinkPageRespDTO.java
+++ project/src/main/java/org/example/shortlink/project/dto/resp/ShortLinkPageRespDTO.java
@@ -1,5 +1,6 @@
 package org.example.shortlink.project.dto.resp;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -58,7 +59,14 @@ public class ShortLinkPageRespDTO {
     /**
      * 有效期
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date validDate;
+
+    /**
+     * 创建时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createTime;
 
     /**
      * 描述
