4785cc308de79737992af89af15e5af480b22ca7
cxlong <3371930743@qq.com>
2024-03-28 17:04:51 +0800

    用户短链接分组分表处理

--- admin/src/test/java/test.java
+++ admin/src/test/java/test.java
@@ -1,24 +1,16 @@
 public class test {
-    public static String SQL = "CREATE TABLE `t_link_%d` (\n" +
+    public static String SQL = "CREATE TABLE `t_group_%d` (\n" +
             "  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
-            "  `domain` varchar(128) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin DEFAULT NULL COMMENT '域名',\n" +
-            "  `short_uri` varchar(8) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin DEFAULT NULL COMMENT '短链接',\n" +
-            "  `full_short_url` varchar(128) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin DEFAULT NULL COMMENT '完整短链接',\n" +
-            "  `origin_url` varchar(1024) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin DEFAULT NULL COMMENT '原始链接',\n" +
-            "  `chick_num` int DEFAULT '0' COMMENT '点击量',\n" +
-            "  `gid` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '分组标识',\n" +
-            "  `favicon` varchar(256) COLLATE utf8mb3_bin DEFAULT NULL COMMENT '网站图标',\n" +
-            "  `enable_status` tinyint(1) DEFAULT NULL COMMENT '启用标识 0:启用 1:未启用',\n" +
-            "  `created_type` tinyint(1) DEFAULT NULL COMMENT '创建类型 0：接口创建 1：控制台创建',\n" +
-            "  `valid_date_type` tinyint(1) DEFAULT NULL COMMENT '有效期类型 0：永久有效 1：自定义',\n" +
-            "  `valid_date` datetime DEFAULT NULL COMMENT '有效期',\n" +
-            "  `describe` varchar(1024) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin DEFAULT NULL COMMENT '描述',\n" +
+            "  `gid` varchar(32) DEFAULT NULL COMMENT '分组标识',\n" +
+            "  `name` varchar(64) DEFAULT NULL COMMENT '分组名称',\n" +
+            "  `username` varchar(256) DEFAULT NULL COMMENT '创建分组用户名',\n" +
+            "  `sort_order` int DEFAULT NULL COMMENT '分组排序',\n" +
             "  `create_time` datetime DEFAULT NULL COMMENT '创建时间',\n" +
             "  `update_time` datetime DEFAULT NULL COMMENT '修改时间',\n" +
             "  `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
-            "  PRIMARY KEY (`id`) USING BTREE,\n" +
-            "  UNIQUE KEY `idx_unique_full_short_url` (`full_short_url`) USING BTREE\n" +
-            ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin ROW_FORMAT=DYNAMIC;";
+            "  PRIMARY KEY (`id`),\n" +
+            "  UNIQUE KEY `idx_unique_username_gid` (`gid`,`username`) USING BTREE\n" +
+            ") ENGINE=InnoDB AUTO_INCREMENT=1771918752495714307 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;";
     public static void main(String[] args) {
         for (int i = 0; i < 16; i++) {
             System.out.printf((SQL) + "%n", i);
