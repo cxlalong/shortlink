b19a4d09503e175c42a3fb3a0a33a9619cf10b79
cxlong <3371930743@qq.com>
2025-01-02 22:43:04 +0800

    feature:开发pv统计

--- project/src/main/java/org/example/shortlink/project/dao/mapper/LinkAccessStatsMapper.java
+++ project/src/main/java/org/example/shortlink/project/dao/mapper/LinkAccessStatsMapper.java
@@ -14,7 +14,7 @@ public interface LinkAccessStatsMapper extends BaseMapper<LinkAccessStatsDO> {
 
     @Insert("INSERT INTO t_link_access_stats ( full_short_url, gid, date, pv, uv, uip, HOUR, weekday, create_time, update_time, del_flag )" +
             "VALUES" +
-            "(#{linkAccessStats.fullShortUrl}, #{linkAccessStats.gid}, #{linkAccessStats.date}, #{linkAccessStats.pv}, #{linkAccessStats.uv}, #{linkAccessStats.uip}, #{linkAccessStats.hour}, #{linkAccessStats.weakday}, NOW(), NOW(), 0 ) " +
+            "(#{linkAccessStats.fullShortUrl}, #{linkAccessStats.gid}, #{linkAccessStats.date}, #{linkAccessStats.pv}, #{linkAccessStats.uv}, #{linkAccessStats.uip}, #{linkAccessStats.hour}, #{linkAccessStats.weekday}, NOW(), NOW(), 0 ) " +
             "ON DUPLICATE KEY UPDATE pv = pv + #{linkAccessStats.pv}," +
             "uv = uv + #{linkAccessStats.uv}," +
             "uip = uip + #{linkAccessStats.uip};")
